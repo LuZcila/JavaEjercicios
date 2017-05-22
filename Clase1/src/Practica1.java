9b5413555c9728864f0f4c7a3e2b1488857282fa
LuZcila <armentanolucila@gmail.com>
2017-05-22 16:32:16 -0300

    Update Practica1.java

--- Clase1/src/Practica1.java
+++ Clase1/src/Practica1.java
@@ -2,10 +2,13 @@
 public class Practica1 {
 
 	
-/*
- * EJERCICIO 1:
- * 
- * */
+    /** EJERCICIO 1)
+     * Se quiere desarrollar un sistema para una clínica médica que administre la información de los pacientes.
+     * a) Definir las variables necesarias para almacenar la información de cada paciente.
+     * Ej: String nombre;
+     * b) Definir una función que reciba por parámetro toda la información del paciente y la almacene en las variables correspondientes
+     * c) Definir una función que imprima por pantalla todos los datos del paciente.
+     */
 	
 	
 	
