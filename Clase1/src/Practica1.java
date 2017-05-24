fc3ed333ddf82ea32cb33ed6f3f2e56018dde6b0
LuZcila <armentanolucila@gmail.com>
2017-05-24 01:24:43 -0300

    Update Practica1.java

--- Clase1/src/Practica1.java
+++ Clase1/src/Practica1.java
@@ -10,6 +10,29 @@ public class Practica1 {
      * c) Definir una función que imprima por pantalla todos los datos del paciente.
      */
 	
+    /** EJERCICIO 2)
+     * Se quiere desarrollar un sistema para un negocio de alfombras que administre la información de sus productos.
+     * a) Definir las variables necesarias para almacenar la información de cada producto (alfombra).
+     * b) Definir una función que reciba por parámetro toda la información del producto y la almacene en las variables correspondientes
+     * c) Definir una función que imprima por pantalla todos los datos del producto. 
+     * Informar precio con y sin i.v.a. Informar el valor de cada cuota si compran con AHORA12 o AHORA18.
+     */
 	
+	
+	/*
+	 EJERCICIO 3)
+	 Responder probando y explicando brevemente
+	 a) ¿Qué pasa si intento almacenar en una variable numérica el resultado de una división por cero?
+	 b) ¿Qué pasa si intento almacenar en una variable de tipo short el resultado de una operación que supera al máximo para ese tipo?
+	 c) ¿Puedo asignarle a una variable de tipo char un caracter entre comillas dobles?
+	 */
+	
+	
+	/*
+	EJERCICIO 4)
+	Dado un número obtenido de forma aleatoria, mostrar por pantalla si es par o impar.
+	Tip: utilizar el operador ternario y %
+	
+	*/
 	
 }
